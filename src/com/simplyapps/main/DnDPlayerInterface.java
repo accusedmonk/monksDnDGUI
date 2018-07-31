44c6e8cf2c2bb3340e6e47770d6280971c30c1cc
rstangel6809 <rstangel6809@email.vccs.edu>
2018-07-31 19:25:12 -0400

    Created some organizational packages.

--- src/com/simplyapps/main/DnDPlayerInterface.java
+++ src/com/simplyapps/main/DnDPlayerInterface.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package dndplayerinterface;
+package com.simplyapps.main;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -19,7 +19,7 @@ public class DnDPlayerInterface extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("com/simplyapps/views/PlayerGUI.fxml"));
         
         Scene scene = new Scene(root);
         
