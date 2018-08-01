22874640ffd76b1bea65fa61883ae219ae25f5f0
rstangel6809 <rstangel6809@email.vccs.edu>
2018-07-31 20:48:43 -0400

    Added a few core classes for characters.

---
+++ src/com/simplyapps/entities/Player.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.simplyapps.entities;
+
+import java.io.Serializable;
+
+
+/**
+ *
+ * @author accusedmonk
+ */
+
+public class Player implements Serializable{
+    
+    private String name;
+    private PlayerClass playerClass;
+    private PlayerBackground playerBackground;
+    private PlayerRace playerRace;
+}
